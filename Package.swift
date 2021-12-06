9eb20b8674bed51a4e71bcebfab798ea3940c07e
Paresh Mittal <paresh@edcast.com>
2021-12-06 11:49:20 +0530

    Add support for iOS 11

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "InputBarAccessoryView",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "InputBarAccessoryView",
