bc80b172ef4530da05f138690022a97061129a67
martinpucik <martin.pucik@me.com>
2022-05-16 15:30:26 +0200

    feat: `additionalInputViewBottomConstraintConstant` in KeyboardManager

--- Sources/KeyboardManager/KeyboardManager.swift
+++ Sources/KeyboardManager/KeyboardManager.swift
@@ -30,47 +30,50 @@ import UIKit
 /// An object that observes keyboard notifications such that event callbacks can be set for each notification
 @available(iOSApplicationExtension, unavailable)
 open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
-    
+
     /// A callback that passes a `KeyboardNotification` as an input
     public typealias EventCallback = (KeyboardNotification)->Void
-    
+
     // MARK: - Properties [Public]
-    
+
     /// A weak reference to a view bounded to the top of the keyboard to act as an `InputAccessoryView`
     /// but kept within the bounds of the `UIViewController`s view
     open weak var inputAccessoryView: UIView?
-    
+
     /// A flag that indicates if a portion of the keyboard is visible on the screen
     private(set) public var isKeyboardHidden: Bool = true
-    
+
     /// A flag that indicates if the additional bottom space should be applied to
     /// the interactive dismissal of the keyboard
     public var shouldApplyAdditionBottomSpaceToInteractiveDismissal: Bool = false
 
+    /// Closure for providing an additional bottom constraint constant for `InputAccessoryView`
+    public var additionalInputViewBottomConstraintConstant: () -> CGFloat = { 0 }
+
     // MARK: - Properties [Private]
-    
+
     /// The additional bottom space specified for laying out the input accessory view
     /// when binding to it
     private var additionalBottomSpace: (() -> CGFloat)?
 
     /// The `NSLayoutConstraintSet` that holds the `inputAccessoryView` to the bottom if its superview
     private var constraints: NSLayoutConstraintSet?
-    
+
     /// A weak reference to a `UIScrollView` that has been attached for interactive keyboard dismissal
     private weak var scrollView: UIScrollView?
-    
+
     /// The `EventCallback` actions for each `KeyboardEvent`. Default value is EMPTY
     private var callbacks: [KeyboardEvent: EventCallback] = [:]
-    
+
     /// The pan gesture that handles dragging on the `scrollView`
     private var panGesture: UIPanGestureRecognizer?
 
     /// A cached notification used as a starting point when a user dragging the `scrollView` down
     /// to interactively dismiss the keyboard
     private var cachedNotification: KeyboardNotification?
-    
+
     // MARK: - Initialization
-    
+
     /// Creates a `KeyboardManager` object an binds the view as fake `InputAccessoryView`
     ///
     /// - Parameter inputAccessoryView: The view to bind to the top of the keyboard but within its superview
@@ -78,25 +81,23 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         self.init()
         self.bind(inputAccessoryView: inputAccessoryView)
     }
-    
+
     /// Creates a `KeyboardManager` object that observes the state of the keyboard
     public override init() {
         super.init()
         addObservers()
     }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+
+    public required init?(coder: NSCoder) { nil }
+
     // MARK: - De-Initialization
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-    
+
     // MARK: - Keyboard Observer
-    
+
     /// Add an observer for each keyboard notification
     private func addObservers() {
         NotificationCenter.default.addObserver(self,
@@ -124,9 +125,9 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
                                                name: UIResponder.keyboardDidChangeFrameNotification,
                                                object: nil)
     }
-    
+
     // MARK: - Mutate Callback Dictionary
-    
+
     /// Sets the `EventCallback` for a `KeyboardEvent`
     ///
     /// - Parameters:
@@ -138,7 +139,7 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         callbacks[event] = callback
         return self
     }
-    
+
     /// Constrains the `inputAccessoryView` to the bottom of its superview and sets the
     /// `.willChangeFrame` and `.willHide` event callbacks such that it mimics an `InputAccessoryView`
     /// that is bound to the top of the keyboard
@@ -147,7 +148,7 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
     /// - Returns: Self
     @discardableResult
     open func bind(inputAccessoryView: UIView, withAdditionalBottomSpace additionalBottomSpace: (() -> CGFloat)? = .none) -> Self {
-        
+
         guard let superview = inputAccessoryView.superview else {
             fatalError("`inputAccessoryView` must have a superview")
         }
@@ -155,17 +156,19 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         self.additionalBottomSpace = additionalBottomSpace
         inputAccessoryView.translatesAutoresizingMaskIntoConstraints = false
         constraints = NSLayoutConstraintSet(
-            bottom: inputAccessoryView.bottomAnchor.constraint(equalTo: superview.bottomAnchor),
+            bottom: inputAccessoryView.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: additionalInputViewBottomConstraintConstant()),
             left: inputAccessoryView.leftAnchor.constraint(equalTo: superview.leftAnchor),
             right: inputAccessoryView.rightAnchor.constraint(equalTo: superview.rightAnchor)
         ).activate()
 
         callbacks[.willShow] = { [weak self] (notification) in
-            let keyboardHeight = notification.endFrame.height
             guard
                 self?.isKeyboardHidden == false,
-                self?.constraints?.bottom?.constant == 0,
-                notification.isForCurrentApp else { return }
+                self?.constraints?.bottom?.constant == self?.additionalInputViewBottomConstraintConstant(),
+                notification.isForCurrentApp
+            else { return }
+
+            let keyboardHeight = notification.endFrame.height
             self?.animateAlongside(notification) {
                 self?.constraints?.bottom?.constant = -keyboardHeight - (additionalBottomSpace?() ?? 0)
                 self?.inputAccessoryView?.superview?.layoutIfNeeded()
@@ -175,7 +178,9 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
             let keyboardHeight = notification.endFrame.height
             guard
                 self?.isKeyboardHidden == false,
-                notification.isForCurrentApp else { return }
+                notification.isForCurrentApp
+            else { return }
+
             self?.animateAlongside(notification) {
                 self?.constraints?.bottom?.constant = -keyboardHeight - (additionalBottomSpace?() ?? 0)
                 self?.inputAccessoryView?.superview?.layoutIfNeeded()
@@ -184,13 +189,13 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         callbacks[.willHide] = { [weak self] (notification) in
             guard notification.isForCurrentApp else { return }
             self?.animateAlongside(notification) { [weak self] in
-                self?.constraints?.bottom?.constant = 0
+                self?.constraints?.bottom?.constant = self?.additionalInputViewBottomConstraintConstant() ?? 0
                 self?.inputAccessoryView?.superview?.layoutIfNeeded()
             }
         }
         return self
     }
-    
+
     /// Adds a `UIPanGestureRecognizer` to the `scrollView` to enable interactive dismissal`
     ///
     /// - Parameter scrollView: UIScrollView
@@ -205,9 +210,9 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         self.scrollView?.addGestureRecognizer(recognizer)
         return self
     }
-    
+
     // MARK: - Keyboard Notifications
-    
+
     /// An observer method called last in the lifecycle of a keyboard becoming visible
     ///
     /// - Parameter notification: NSNotification
@@ -216,7 +221,7 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         guard let keyboardNotification = KeyboardNotification(from: notification) else { return }
         callbacks[.didShow]?(keyboardNotification)
     }
-    
+
     /// An observer method called last in the lifecycle of a keyboard becoming hidden
     ///
     /// - Parameter notification: NSNotification
@@ -227,7 +232,7 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         callbacks[.didHide]?(keyboardNotification)
         cachedNotification = nil
     }
-    
+
     /// An observer method called third in the lifecycle of a keyboard becoming visible/hidden
     ///
     /// - Parameter notification: NSNotification
@@ -237,7 +242,7 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         callbacks[.didChangeFrame]?(keyboardNotification)
         cachedNotification = keyboardNotification
     }
-    
+
     /// An observer method called first in the lifecycle of a keyboard becoming visible/hidden
     ///
     /// - Parameter notification: NSNotification
@@ -247,7 +252,7 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         callbacks[.willChangeFrame]?(keyboardNotification)
         cachedNotification = keyboardNotification
     }
-    
+
     /// An observer method called second in the lifecycle of a keyboard becoming visible
     ///
     /// - Parameter notification: NSNotification
@@ -257,7 +262,7 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         guard let keyboardNotification = KeyboardNotification(from: notification) else { return }
         callbacks[.willShow]?(keyboardNotification)
     }
-    
+
     /// An observer method called second in the lifecycle of a keyboard becoming hidden
     ///
     /// - Parameter notification: NSNotification
@@ -267,15 +272,15 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         callbacks[.willHide]?(keyboardNotification)
         cachedNotification = nil
     }
-    
+
     // MARK: - Helper Methods
-    
+
     private func animateAlongside(_ notification: KeyboardNotification, animations: @escaping ()->Void) {
         UIView.animate(withDuration: notification.timeInterval, delay: 0, options: [notification.animationOptions, .allowAnimatedContent, .beginFromCurrentState], animations: animations, completion: nil)
     }
-    
+
     // MARK: - UIGestureRecognizerDelegate
-    
+
     /// Starts with the cached `KeyboardNotification` and calculates a new `endFrame` based
     /// on the `UIPanGestureRecognizer` then calls the `.willChangeFrame` `EventCallback` action
     ///
@@ -287,14 +292,15 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
             case .changed = recognizer.state,
             let view = recognizer.view,
             let window = UIApplication.shared.windows.first
-            else { return }
+        else { return }
 
         guard
-            // if there's no difference in frames for the `cachedNotification`, no adjustment is necessary. This is true when the keyboard is completely dismissed, or our pan doesn't intersect below the keyboard
+            // if there's no difference in frames for the `cachedNotification`, no adjustment is necessary.
+            // This is true when the keyboard is completely dismissed, or our pan doesn't intersect below the keyboard
             keyboardNotification.startFrame != keyboardNotification.endFrame,
             // when the width of the keyboard from endFrame is smaller than the width of scrollView manager is tracking
             // with panGesture, we can assume the keyboard is floatig ahd updating inputAccessoryView is not necessary
-                keyboardNotification.endFrame.width >= view.frame.width
+            keyboardNotification.endFrame.width >= view.frame.width
         else {
             return
         }
@@ -307,26 +313,23 @@ open class KeyboardManager: NSObject, UIGestureRecognizerDelegate {
         keyboardNotification.endFrame = frame
 
         var yCoordinateDirectlyAboveKeyboard = -frame.height
-
-        if shouldApplyAdditionBottomSpaceToInteractiveDismissal,
-           let additionalBottomSpace = additionalBottomSpace {
+        if shouldApplyAdditionBottomSpaceToInteractiveDismissal, let additionalBottomSpace = additionalBottomSpace {
             yCoordinateDirectlyAboveKeyboard -= additionalBottomSpace()
         }
 
         /// If a tab bar is shown, letting this number becoming > 0 makes it so the accessoryview disappears below the tab bar. setting the max value to 0 prevents that
-        let aboveKeyboardAndAboveTabBar = min(0, yCoordinateDirectlyAboveKeyboard)
+        let aboveKeyboardAndAboveTabBar = min(additionalInputViewBottomConstraintConstant(), yCoordinateDirectlyAboveKeyboard)
         self.constraints?.bottom?.constant = aboveKeyboardAndAboveTabBar
         self.inputAccessoryView?.superview?.layoutIfNeeded()
     }
-    
+
     /// Only receive a `UITouch` event when the `scrollView`'s keyboard dismiss mode is interactive
     open func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
         return scrollView?.keyboardDismissMode == .interactive
     }
-    
+
     /// Only recognice simultaneous gestures when its the `panGesture`
     open func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return gestureRecognizer === panGesture
     }
-    
 }
